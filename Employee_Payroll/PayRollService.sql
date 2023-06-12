8f82c074c5511829d1150f745482c55e5d0d4298
Harshita <chandaliyaharshita222@gmail.com>
2023-06-12 22:11:10 +0530

    Retrieve particular data

--- Employee_Payroll/PayRollService.sql
+++ Employee_Payroll/PayRollService.sql
@@ -15,3 +15,7 @@ VALUES ('Dhoni', 87000, '2002-05-02'), ('UV', 90000, '1991-06-04');
 INSERT INTO EmployeePayRoll VALUES ('Sachin', 78900, '2019-02-04'), ('Kumar', 45000, '2022-05-05');
 ----------------UC4---------------
 select * from EmployeePayRoll;
+----------------UC5---------------
+select Salary from EmployeePayRoll where Name = 'UV';
+select * from EmployeePayRoll where StartDate = '2019-02-04';
+select * from EmployeePayRoll where StartDate between '2000-01-01' And getDate();
