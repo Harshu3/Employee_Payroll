2e08e03d724f2db0d0b8ac0efd95c6abbbe72908
Harshita <chandaliyaharshita222@gmail.com>
2023-06-12 22:03:37 +0530

    Retrieve all data

--- Employee_Payroll/PayRollService.sql
+++ Employee_Payroll/PayRollService.sql
@@ -13,3 +13,5 @@ INSERT INTO EmployeePayRoll (Name, Salary, StartDate)
 VALUES ('Dhoni', 87000, '2002-05-02'), ('UV', 90000, '1991-06-04');
 
 INSERT INTO EmployeePayRoll VALUES ('Sachin', 78900, '2019-02-04'), ('Kumar', 45000, '2022-05-05');
+----------------UC4---------------
+select * from EmployeePayRoll;
