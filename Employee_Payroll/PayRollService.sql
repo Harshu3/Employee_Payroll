eeade38e9af1e9b238c469b724c8af1f1bba98e4
Harshita <chandaliyaharshita222@gmail.com>
2023-06-12 19:38:25 +0530

    Create table in database

--- Employee_Payroll/PayRollService.sql
+++ Employee_Payroll/PayRollService.sql
@@ -1,3 +1,10 @@
 ----------------UC1---------------
 create database PayRollService;
 use PayRollService;
+----------------UC2---------------
+CREATE TABLE EmployeePayRoll (
+	Id int primary key identity(1,1),
+	Name varchar(255),
+	Salary float,
+	StartDate date
+);
